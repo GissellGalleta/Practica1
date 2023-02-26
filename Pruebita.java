5c239c01b718b9011ed51fc94c0f4e9b9549e2fb
gissell <l21020451@veracruz.tecnm.mx>
2023-02-25 22:05:38 -0600

    adios

--- Pruebita.java
+++ Pruebita.java
@@ -5,5 +5,7 @@ public class Pruebita {
         System.out.println("Hola, esta es una prueba");
         System.out.println("Esta es una modificacion de Gissell");
         System.out.println("Hola, Nadia");
+
+        System.out.print("ADIOS TRIPONA");
     }
 }
