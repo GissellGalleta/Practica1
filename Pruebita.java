97d9d17c70d70ab650ef0aec815137fde701cf2b
Jose Antonio Mendoza Hernandez <l21020409@veracruz.tecnm.mx>
2023-02-26 00:13:26 -0600

    Modificacion Jose

--- Pruebita.java
+++ Pruebita.java
@@ -3,5 +3,7 @@ public class Pruebita {
     public static void main(String[] args) {
 
         System.out.println("Hola, esta es una prueba");
+        System.out.println("Modificacion Jose");
+
     }
 }
