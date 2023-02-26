2b7dd0892bc6346dfe6696a7de95ffc3483e883c
Karol <L21020442@veracruz.tecnm.mx>
2023-02-25 21:02:15 -0600

    Prueba Karol

--- Pruebita.java
+++ Pruebita.java
@@ -3,5 +3,6 @@ public class Pruebita {
     public static void main(String[] args) {
 
         System.out.println("Hola, esta es una prueba");
+        System.out.println("Hola Modificacion de Karol");
     }
 }
